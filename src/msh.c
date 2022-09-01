790d93e6b2eebb850a929f8d4141104650390b6a
Simon Schneemelcher <schneemelchercom@gmail.com>
2022-09-01 16:15:16 +0200

    add command struct

--- src/msh.c
+++ src/msh.c
@@ -4,14 +4,6 @@
 #include <string.h>
 #include <unistd.h>
 
-enum {
-  MAX_INPUT = 1024,
-  MAX_PATH = 128,
-  MAX_PROMPT = 256,
-  MAX_ARGS = 16,
-  MAX_CMD = 32
-};
-
 int main(int argc, char **argv) {
   shell_loop();
   return 0;
@@ -25,7 +17,18 @@ int shell_loop() {
     getcwd(cwd, MAX_PATH);
     print_prompt(cwd, prompt);
     fgets(input, MAX_INPUT, stdin);
-    parse_input(input);
+    struct command cmd;
+    if (parse_input(input, &cmd) > 0) {
+      // fork and exec
+      const int pid = fork();
+      if (pid != 0) {
+        int result = 0;
+        waitpid(pid, &result, 0);
+      } else {
+        execvp(cmd.argv[0], cmd.arg_ptrs);
+        return 0;
+      }
+    }
   } while (!(input[0] == 'e' && input[1] == 'x' && input[2] == 'i' &&
              input[3] == 't'));
 
@@ -39,7 +42,7 @@ void print_prompt(char *cwd, char *prompt) {
   printf("%s $ ", cwd);
 }
 
-int parse_input(char *input) {
+int parse_input(char *input, struct command *cmd) {
 
   /* we need to parse the line we got
    * execvp needs a char* with the command,
@@ -47,36 +50,31 @@ int parse_input(char *input) {
    * the command itself
    */
 
-  char argument_list[MAX_ARGS][MAX_CMD] = {0};
-  char *argument_ptrs[MAX_ARGS] = {0};
   int i = 0;
   int word = 0;
   int word_idx = 0;
-  while (i < MAX_INPUT && word_idx < MAX_CMD && word < MAX_ARGS && input[i] != 0) {
-    argument_list[word][word_idx] = input[i];
+  while (i < MAX_INPUT - 1 && word_idx < MAX_CMD - 1 && word < MAX_ARGS - 1 &&
+         input[i] != 0) {
+    cmd->argv[word][word_idx] = input[i];
     i += 1;
     word_idx += 1;
     if (input[i] == ' ') {
-      argument_ptrs[word] = argument_list[word];
+      cmd->argv[word][word_idx] = '\0';
+      cmd->arg_ptrs[word] = cmd->argv[word];
       i += 1;
       word += 1;
       word_idx = 0;
     } else if (input[i] == '\n') {
-      argument_ptrs[word] = argument_list[word];
+      cmd->argv[word][word_idx] = '\0';
+      cmd->arg_ptrs[word] = cmd->argv[word];
+      if (i != 0)
+        word += 1;
       break;
     }
   }
-
-  if (argument_list[0][0] != 0) {
-    // fork and exec
-    const int pid = fork();
-    if (pid != 0) {
-      int result = 0;
-      waitpid(pid, &result, 0);
-    } else {
-      execvp(argument_list[0], argument_ptrs);
-      return 0;
-    }
-  }
-  return 0;
+  cmd->arg_ptrs[word] = NULL;
+  cmd->argc = word;
+  cmd->bg = 0;
+  cmd->builtin = 0;
+  return word;
 }
--- src/msh.h
+++ src/msh.h
@@ -1,3 +1,19 @@
+enum {
+  MAX_INPUT = 1024,
+  MAX_PATH = 128,
+  MAX_PROMPT = 256,
+  MAX_ARGS = 16,
+  MAX_CMD = 32
+};
+
+struct command {
+  int argc;
+  char argv[MAX_ARGS][MAX_CMD];
+  char** arg_ptrs;
+  int bg;
+  int builtin;
+};
+
 int shell_loop();
 void print_prompt(char* cwd, char* prompt);
-int parse_input(char* input);
+int parse_input(char *input, struct command *cmd);
