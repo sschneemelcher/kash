83ca79ebdc6909c2cf883a5a6b6f338611050c7d
Simon Schneemelcher <schneemelchercom@gmail.com>
2022-09-01 12:33:11 +0200

    add shell loop

--- src/msh.c
+++ src/msh.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "msh.h"
 
-int main() {
-    printf("Hello World!");
+const int MAX_INPUT = 1024;
+
+int main(int argc, char** argv) {
+    shell_loop();
+    return 0;
+}
+
+int shell_loop() {
+    char* prompt = "> ";
+    char input[MAX_INPUT] = { 0 };
+
+    do {
+        printf("%s", prompt);
+        fgets(input, MAX_INPUT, stdin);
+        // just echo the input back for now
+        printf("%s", input);
+
+    } while (!(input[0] == 'e' && input[1] == 'x' && input[2] == 'i' && input[3] == 't'));
+
     return 0;
 }
