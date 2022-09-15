ee858dd9be665fe89578246648c06eb2bca170d8
Simon Schneemelcher <schneemelchercom@gmail.com>
2022-09-15 15:35:13 +0200

    switch to open/close

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/errno.h>
+#include <fcntl.h>
 
 void intHandler(int dummy) {}
 
@@ -26,19 +27,19 @@ int main(int argc, char **argv, char **envp) {
   }
 
   if (!isatty(STDIN_FILENO)) {
-    shell_loop(envp, NONINTERACTIVE, stdin);
+    shell_loop(envp, NONINTERACTIVE, STDIN_FILENO);
   } else if (optind >= argc) {
-    shell_loop(envp, INTERACTIVE, NULL);
+    shell_loop(envp, INTERACTIVE, 0);
   } else {
     int i = optind;
     while (i < argc) {
-      FILE *fp = fopen(argv[i], "r");
+      int fp = open(argv[i], O_RDONLY);
       if (fp == 0) {
         printf("-kash: unable to open file '%s': %s\n", argv[i], strerror(errno));
         return 1;
       } else {
         shell_loop(envp, NONINTERACTIVE, fp);
-        fclose(fp);
+        close(fp);
       }
       i++;
     }
@@ -46,7 +47,7 @@ int main(int argc, char **argv, char **envp) {
   }
 }
 
-int shell_loop(char **env, enum session_t sess, FILE *inp) {
+int shell_loop(char **env, enum session_t sess, int inp) {
   struct command cmd;
   char input[MAX_INPUT + 1] = "";
   char prompt[MAX_PROMPT + 1] = "";
@@ -56,7 +57,7 @@ int shell_loop(char **env, enum session_t sess, FILE *inp) {
       print_prompt(prompt);
       handle_keys(input);
     } else {
-      fread(input, MAX_INPUT, 1, inp);
+      read(inp, input, MAX_INPUT);
       input[MAX_INPUT] = 0;
     }
     char *line_ret;
--- src/main.h
+++ src/main.h
@@ -1,2 +1,2 @@
-int shell_loop(char **env, enum session_t sess, FILE* inp);
+int shell_loop(char **env, enum session_t sess, int inp);
 void intHandler(int dummy);
