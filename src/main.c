8359c4d055f39071adc6d539f59e89472b91fa61
Simon Schneemelcher <schneemelchercom@gmail.com>
2022-09-23 13:22:35 +0200

    remove interactive use

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include "utils.h"
 #include <stdio.h>
 #include "main.h"
-#include "keys.h"
 #include "parse.h"
 #include "prompt.h"
 #include "run.h"
@@ -9,27 +8,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/errno.h>
-#include <fcntl.h>
 
 void intHandler(int dummy) {}
 
 int main(int argc, char **argv, char **envp) {
-  signal(SIGINT, intHandler);
-  int opt, sess = INTERACTIVE;
-  char *inp;
-  while ((opt = getopt(argc, argv, "c:")) != -1) {
-    switch (opt) {
-    case 'c':
-      inp = optarg;
-      sess = NONINTERACTIVE;
-      break;
-    }
-  }
-
   if (!isatty(STDIN_FILENO)) {
     shell_loop(envp, NONINTERACTIVE, STDIN_FILENO);
-  } else if (optind >= argc) {
-    shell_loop(envp, INTERACTIVE, 0);
   } else {
     int i = optind;
     while (i < argc) {
@@ -49,33 +33,11 @@ int main(int argc, char **argv, char **envp) {
 
 int shell_loop(char **env, enum session_t sess, int inp) {
   struct command cmd;
-  char history[MAX_HISTORY + 1][MAX_INPUT + 1];
-  for (int i = 0; i <= MAX_HISTORY; i++) {
-    history[i][0] = '\0';
-  }
   char input[MAX_INPUT + 1] = "";
-  char prompt[MAX_PROMPT + 1] = "";
-
-  int history_idx = 0;
-  while (1) {
-    if (sess == INTERACTIVE) {
-      print_prompt(prompt);
-      handle_keys(input, history, history_idx);
-    } else {
-      read(inp, input, MAX_INPUT);
-      input[MAX_INPUT] = 0;
-    }
-    strcpy(history[history_idx], input);
-    history_idx = (history_idx + 1) % MAX_HISTORY;
-    char *line_ret;
-    for (char *line = strtok_r(input, "\n;", &line_ret); line;
-         line = strtok_r(NULL, "\n;", &line_ret)) {
-      parse_input(line, &cmd);
-      if(run(cmd, env))
-          return EXIT_SUCCESS;
-    }
-    if (sess == NONINTERACTIVE)
-      break;
-  }
+  read(inp, input, MAX_INPUT);
+  input[MAX_INPUT] = 0;
+  parse_input(input, &cmd);
+  if(run(cmd, env))
+      return EXIT_FAILURE;
   return EXIT_SUCCESS;
 }
